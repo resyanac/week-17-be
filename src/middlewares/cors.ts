57e2bb38aad5cc5564edaac07272dec97f863aff
Resyana Cahyanita <resyanac@gmail.com>
2023-10-07 14:29:29 +0800

    set domain 2

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,4 +1,13 @@
 const clientAccess = {
+    // limitedClient : {
+    //     // origin: ['https://week15-rpb-x.netlify.app', 'https://week15-rpb-x.netlify.app/*','https://week15-rpb-y.netlify.app', 'https://week15-rpb-y.netlify.app/*'],
+    //     origin: ['http://localhost:5173', 'http://localhost:5173/*', 'http://localhost:5174', 'http://localhost:5174/*'],
+    //     methods: ['GET', 'POST']
+    // },
+    // globalClient : {
+    //     origin: ['http://localhost:5174', 'http://localhost:5174/*'],
+    //     methods: ['PATCH', 'DELETE']
+    // }
     limitedClient : {
         // origin: ['https://week15-rpb-x.netlify.app', 'https://week15-rpb-x.netlify.app/*','https://week15-rpb-y.netlify.app', 'https://week15-rpb-y.netlify.app/*'],
         origin: ['https://resyanac-week-15-client-x.netlify.app/', 'https://resyanac-week-15-client-x.netlify.app/*', 'https://resyanac-week-15-client-y.netlify.app', 'https://resyanac-week-15-client-y.netlify.app/*'],
