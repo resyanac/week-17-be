4ebc6f2c1a4c6373b5a820a0da7f7b6b812cb680
Resyana Cahyanita <resyanac@gmail.com>
2023-10-07 17:01:19 +0800

    again

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -10,11 +10,11 @@ const clientAccess = {
     // }
     limitedClient : {
         // origin: ['https://week15-rpb-x.netlify.app', 'https://week15-rpb-x.netlify.app/*','https://week15-rpb-y.netlify.app', 'https://week15-rpb-y.netlify.app/*'],
-        origin: ['https://resyanac-week-15-client-x.netlify.app/', 'https://resyanac-week-15-client-x.netlify.app/*', 'https://resyanac-week-15-client-y.netlify.app', 'https://resyanac-week-15-client-y.netlify.app/*'],
+        origin: ['https://resyanac-week-15-client-x.netlify.app', 'https://resyanac-week-15-client-x.netlify.app/*', 'https://resyanac-week-15-client-y.netlify.app', 'https://resyanac-week-15-client-y.netlify.app/*'],
         methods: ['GET', 'POST']
     },
     globalClient : {
-        origin: ['https://resyanac-week-15-client-x.netlify.app/', 'https://resyanac-week-15-client-x.netlify.app/*'],
+        origin: ['https://resyanac-week-15-client-x.netlify.app', 'https://resyanac-week-15-client-x.netlify.app/*'],
         methods: ['PATCH', 'DELETE']
     }
 }
