50880d9ace47f22d66f94816a249e83c7f72d319
Resyana Cahyanita <resyanac@gmail.com>
2023-10-22 15:54:35 +0800

    bismillah  commit

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -8,6 +8,9 @@ import corsMiddleware from '../middlewares/cors'
 
 const userRoutes = express.Router()
 
+userRoutes.options('/v1/register')
+userRoutes.options('/v1/login')
+userRoutes.options('/v1/logout')
 userRoutes.post('/v1/register',  register);
 userRoutes.post('/v1/login', login);
 userRoutes.post('/v1/logout', logout)
